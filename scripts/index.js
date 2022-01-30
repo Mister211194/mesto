90dd01f3e18332723f31ea606e0a70784bab3d5a
EVGENY OSHKIN <garaz777@gmail.com>
2022-01-30 04:09:19 +0300

    feat: Доавить функции добавления карточек, попап добавления карточк, функция лайка, функция удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // Добавление выборки DOM элементов
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const popupElement = document.querySelector('.popup');
+const popupElement = document.querySelector('.profile-popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__closed');
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.getElementById('input-popup-name');
@@ -43,3 +43,98 @@ popupCloseButtonElement.addEventListener('click', closePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+const popupAddCards = document.querySelector('.add-cards-popup');
+const buttonAddCards = document.querySelector('.profile__add-button');
+const closeButtonAddCardsPopup = popupAddCards.querySelector('.popup__closed');
+const inputAddCardTitile = popupAddCards.querySelector('#input-popup-title');
+const inputAddCardUrl = popupAddCards.querySelector('#input-popup-url');
+const formElementAdd = popupAddCards.querySelector('.popup__form');
+
+// Открытие попап добавления карточек с фото
+function openAddCardsPopup() {
+    inputAddCardTitile.value = '';
+    inputAddCardUrl.value = '';
+    popupAddCards.classList.add('popup_open');
+}
+// Закрытие попап добавления карточек с фото
+function closeAddCardsPopup() {
+    popupAddCards.classList.remove('popup_open');
+
+}
+
+// Слушатели на попап добавления карточек
+buttonAddCards.addEventListener('click', openAddCardsPopup);
+closeButtonAddCardsPopup.addEventListener('click', closeAddCardsPopup);
+
+import initialCards from './inital_card.js';
+
+const template = document.querySelector('.template').content;
+const cardsSection = document.querySelector('.elements__grid-cards');
+
+function addAllCards() {
+    initialCards.forEach(addCards);
+}
+
+// функция загрузки карточек
+function addCards(value) {
+    const newCard = template.cloneNode(true); // Клонируем содержимое тимплейта
+    const cardImage = newCard.querySelector('.elements__card-image');
+    const cardTitle = newCard.querySelector('.elements__card-title');
+    cardImage.src = value.link;
+    cardTitle.textContent = value.name;
+    cardImage.setAttribute("alt", value.name);
+
+    addListeners(newCard)
+    cardsSection.appendChild(newCard);
+}
+
+function addListeners(el) {
+    el.querySelector('.elements__like-button').addEventListener('click', likeElementActive);
+    el.querySelector('.elements__delete-card').addEventListener('click', cardDelete);
+}
+
+
+
+// Лайк
+function likeElementActive(event) {
+    event.target.classList.toggle('elements__like-button_active')
+}
+
+function cardDelete(event) {
+    event.target.closest('.elements__card').remove();
+}
+
+addAllCards()
+
+
+// Функция добавления карточки
+function formSubmitAddCard(event) {
+    event.preventDefault();
+
+    const newItem = template.cloneNode(true);
+    newItem.querySelector('.elements__card-title').textContent = inputAddCardTitile.value;
+    newItem.querySelector('.elements__card-image').src = inputAddCardUrl.value;
+    newItem.querySelector('.elements__card-image').setAttribute("alt", inputAddCardTitile.value);
+
+    addEvList(newItem);
+    cardsSection.prepend(newItem);
+
+    closeAddCardsPopup()
+}
+
+formElementAdd.addEventListener('submit', formSubmitAddCard);
+
+function addEvList(item) {
+    item.querySelector('.elements__delete-card').addEventListener('click', deleteItem);
+    item.querySelector('.elements__like-button').addEventListener('click', likeItem);
+}
+
+function deleteItem(event) {
+    event.target.closest('.elements__card').remove();
+}
+
+function likeItem(event) {
+    event.target.classList.toggle('elements__like-button_active')
+}
